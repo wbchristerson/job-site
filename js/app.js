2a78cff1567b9ce2bc1d46bb4890142e1ab92041
Will Christerson <wbchristerson@gmail.com>
2018-01-08 08:26:08 -0500

    Implement border around current webpage section title

--- js/app.js
+++ js/app.js
@@ -14,22 +14,28 @@ $( '.topic-label' ).click(function() {
 
 function setPage(elem) {
   let content = elem.text();
+  let currTab = $( '.trigger' ).children().first();
   $( 'body' ).css('background', '#ffffff')
 
   $( '.projects-page' ).addClass( 'hide-content' );
   $( '.about' ).addClass( 'hide-content' );
   $( '.contact' ).addClass( 'hide-content' );
 
+  $( '.page-link' ).removeClass( 'select-border' );
+
   if (content === 'Home') {
     $( '.about' ).removeClass( 'hide-content' );
   }
   else if ((content === 'Projects') || (content === 'Will Christerson')) {
     $( '.projects-page' ).removeClass( 'hide-content' );
+    currTab = currTab.next();
   }
   else if (content === 'Contact') {
     $( '.contact' ).removeClass( 'hide-content' );
     $( 'body' ).css('background', 'linear-gradient(160deg, #66afdd 0%, #dddb66 100%)');
+    currTab = currTab.next().next();
   }
+  currTab.addClass( 'select-border' );
 }
 
 
