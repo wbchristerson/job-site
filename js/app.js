281885df735beb6fdc4a36fdd614d07633e289d7
Will Christerson <wbchristerson@gmail.com>
2018-01-08 07:58:11 -0500

    Change tab title

--- js/app.js
+++ js/app.js
@@ -28,10 +28,7 @@ function setPage(elem) {
   }
   else if (content === 'Contact') {
     $( '.contact' ).removeClass( 'hide-content' );
-    // $( 'body' ).css('background-color', 'red');
     $( 'body' ).css('background', 'linear-gradient(160deg, #66afdd 0%, #dddb66 100%)');
-
-    // background: linear-gradient(160deg, #02ccba 0%, #aa7ecd 100%);
   }
 }
 
@@ -39,26 +36,6 @@ function setPage(elem) {
 $( '.page-link' ).click(function(event) {
   event.preventDefault();
   setPage($( this ));
-  // let content = $( this ).text();
-  // $( 'body' ).css('background', '#ffffff')
-  //
-  // $( '.home' ).addClass( 'hide-content' );
-  // $( '.about' ).addClass( 'hide-content' );
-  // $( '.contact' ).addClass( 'hide-content' );
-  //
-  // if (content === 'Home') {
-  //   $( '.home' ).removeClass( 'hide-content' );
-  // }
-  // else if (content === 'About') {
-  //   $( '.about' ).removeClass( 'hide-content' );
-  // }
-  // else if (content === 'Contact') {
-  //   $( '.contact' ).removeClass( 'hide-content' );
-  //   // $( 'body' ).css('background-color', 'red');
-  //   $( 'body' ).css('background', 'linear-gradient(160deg, #66afdd 0%, #dddb66 100%)');
-  //
-  //   // background: linear-gradient(160deg, #02ccba 0%, #aa7ecd 100%);
-  // }
 });
 
 
